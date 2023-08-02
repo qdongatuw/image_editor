abbf5c039fb48e23fd1ae39abe781bfe0042b3b0
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-08-02 12:57:13 -0500

    1

--- image_editor/lib/main.dart
+++ image_editor/lib/main.dart
@@ -1,74 +1,197 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
 }
 
+String titleOfApp = "Image Editor";
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Image Editor'),
-        ),
-        body: SplitView(),
+      title: 'Image Processing App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ImageGridPage(),
     );
   }
 }
 
-class SplitView extends StatefulWidget {
+class ImageGridPage extends StatefulWidget {
   @override
-  _SplitViewState createState() => _SplitViewState();
+  _ImageGridPageState createState() => _ImageGridPageState();
 }
 
-class _SplitViewState extends State<SplitView> {
-  List<File> images = []; // List to store selected images
+class _ImageGridPageState extends State<ImageGridPage> {
+  List<File> selectedImages = [];
+  List<File> allImages = [];
 
-  Future<void> _addImage() async {
-    final picker = ImagePicker();
-    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
-    if (pickedImage != null) {
+    void _showImagePicker() async {
+    List<XFile>? pickedFile = await ImagePicker().pickMultiImage(imageQuality: 80);
+    if (pickedFile != null) {
       setState(() {
-        images.add(File(pickedImage.path));
+        allImages.addAll(pickedFile.map((file) => File(file.path)).toList());
       });
     }
   }
 
+  void _clearIamges(){
+    setState(() {
+      allImages.clear();
+      selectedImages.clear();
+    });
+    }
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: images.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: Container(
-                  width: 100,
-                  child: Image.file(images[index], fit: BoxFit.contain),
-                  ),
-                title: Text(images[index].path),
-                trailing: IconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: () {
-                    setState(() {
-                      images.removeAt(index);
-                    });
-                  },
-                ),
-              );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titleOfApp),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+        ),
+        itemCount: allImages.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                if (selectedImages.contains(allImages[index])) {
+                  selectedImages.remove(allImages[index]);
+                } else {
+                  selectedImages.add(allImages[index]);
+                }
+              });
             },
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Image.file(
+                    allImages[index],
+                    width: 120,
+                    height: 120,
+                    fit: BoxFit.contain,
+                  ),
+
+                  Checkbox(
+                    value: selectedImages.contains(allImages[index]),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value!) {
+                          selectedImages.add(allImages[index]);
+                        } else {
+                          selectedImages.remove(allImages[index]);
+                        }
+                      });
+                    },
+                  ),
+
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+        onPressed: _showImagePicker,
+        child: const Icon(Icons.add),
+      ),
+      const SizedBox(width: 16), 
+          FloatingActionButton(
+            onPressed: _clearIamges,
+            child: const Icon(Icons.clear),
           ),
-        ),
-        ElevatedButton(
-          onPressed: _addImage,
-          child: Text('Add Image'),
-        ),
-      ],
+        ],
+      ),
+       
+      bottomNavigationBar: selectedImages.isNotEmpty
+          ? BottomAppBar(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('${selectedImages.length} images selected'),
+                    ElevatedButton(
+                      onPressed: () {
+                        _showBottomSheet(context);
+                      },
+                      child: Text('Process Images'),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : null,
+    );
+  }
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.rotate_left),
+                title: Text('Rotate'),
+                onTap: () {
+                  // Rotate selected images logic here
+                  for (var toProcess in selectedImages){
+                    setState(() {
+                      
+                    });
+                  }
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.crop),
+                title: Text('Trim'),
+                onTap: () {
+                  // Rotate selected images logic here
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.brush),
+                title: Text('Re-color'),
+                onTap: () {
+                  // Rotate selected images logic here
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.contrast),
+                title: Text('Grayscale'),
+                onTap: () {
+                  // Rotate selected images logic here
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.filter),
+                title: Text('Apply Filter'),
+                onTap: () {
+                  // Apply filter to selected images logic here
+                  Navigator.pop(context);
+                },
+              ),
+              // Add more processing options here
+            ],
+          ),
+        );
+      },
     );
   }
 }
